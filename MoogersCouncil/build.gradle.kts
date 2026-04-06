9b28314ab91ca2d68bd731932479ea1fb96e2098
Muhammad Shahbaz Aziz Khan <27100049@lums.edu.pk>
2026-04-06 11:47:13 +0500

    fix: converted submodule to regular folder with actual code

---
+++ MoogersCouncil/build.gradle.kts
@@ -0,0 +1,17 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    id("com.google.gms.google-services") version "4.4.4" apply false
+}
+
+/*claude saying add this buildscript {
+    dependencies {
+        classpath 'com.google.gms:google-services:4.4.0'
+    }
+}
+
+plugins {
+    id 'com.android.application' version '8.2.0' apply false
+}
+pata nahi kyu
+ */
